0b20275513e80490e2c97b53d273abecb133d7f1
martinn <martinn@innova.co.ke>
2022-03-03 20:56:09 +0300

    Developing Ordering.Infrastructure Layer in Clean Architecture - Persistence

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        // Method Creates database and seeds the data
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContext> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed databasae associated with context {DbContextName}", typeof(OrderContext).Name);
+            }
+        }
+
+        // Method seeds data for the Orders table
+        public static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() { UserName = "swn", FirstName = "Mehmet", LastName = "Ozkaya", EmailAddress = "ezozkme@gmail.com",
+                    AddressLine = "Bahcelievler", Country = "Turkey", TotalPrice = 350 }
+            };
+        }
+    }
+}
