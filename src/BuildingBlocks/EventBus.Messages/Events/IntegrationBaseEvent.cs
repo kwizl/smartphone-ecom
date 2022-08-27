a803d3adc2a41d05248f3f400e6a56f7d36f738e
martinn <martinn@innova.co.ke>
2022-08-27 14:16:13 +0300

    Developing BuildingBlocks EventBus.Messages Library

---
+++ src/BuildingBlocks/EventBus.Messages/Events/IntegrationBaseEvent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Messages.Events
+{
+    public class IntegrationBaseEvent
+    {
+        public Guid _ID { get; set; }
+        public DateTime _CreationDate { get; private set; }
+
+        public IntegrationBaseEvent()
+        {
+            _ID = Guid.NewGuid();
+            _CreationDate = DateTime.UtcNow;
+        }
+
+        public IntegrationBaseEvent(Guid ID, DateTime CreateDate)
+        {
+            _ID = ID;
+            _CreationDate = CreateDate;
+        }
+    }
+}
