e7c51853039265102614eb8a7fa90c9c80ca706f
martinn <martinn@innova.co.ke>
2022-09-10 12:29:49 +0300

    Develop DTO models for API Aggregator Operations

---
+++ src/ApiGateways/Shopping.Aggregator/Models/ShoppingModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Shopping.Aggregator.Models
+{
+    public class ShoppingModel
+    {
+        public string UserName { get; set; }
+        
+        public BasketModel BasketModel { get; set; }
+        
+        public IEnumerable<OrderResponseModel> Orders { get; set; }
+    }
+}
