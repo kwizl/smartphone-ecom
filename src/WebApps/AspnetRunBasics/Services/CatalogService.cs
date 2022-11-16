546fa478f374b89755a2bd97e12f0f71481b3d02
martinn <martinn@innova.co.ke>
2022-11-16 18:59:28 +0300

    Refactoring of AspnetBasics Shopping Web Application Microservices

---
+++ src/WebApps/AspnetRunBasics/Services/CatalogService.cs
@@ -0,0 +1,37 @@
+﻿using AspnetRunBasics.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.Services
+{
+    public class CatalogService : ICatalogService
+    {
+        private readonly HttpClient _client;
+
+        public CatalogService(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public Task<CatalogModel> CreateCatalog(CatalogModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<CatalogModel>> GetCatalog()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<CatalogModel> GetCatalog(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
