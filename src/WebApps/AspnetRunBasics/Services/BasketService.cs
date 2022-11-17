92b061b5ff249849368d725a9f07f731a1929f78
martinn <martinn@innova.co.ke>
2022-11-17 21:40:08 +0300

    Register Http Client Factory for Consuming Api Gateway in AspnetBasics Shopping

---
+++ src/WebApps/AspnetRunBasics/Services/BasketService.cs
@@ -0,0 +1,33 @@
+﻿using AspnetRunBasics.Interfaces;
+using AspnetRunBasics.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly HttpClient _client;
+
+        public BasketService(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public Task CheckoutBasket(BasketCheckoutModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<BasketModel> GetBasket(string userName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<BasketModel> UpdateBasket(BasketModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/WebApps/AspnetRunBasics/Services/OrderService.cs
@@ -0,0 +1,22 @@
+﻿using AspnetRunBasics.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly HttpClient _client;
+
+        public OrderService(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
